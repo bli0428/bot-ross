b5aaa7d63501e9f2d1225206f41415d5fbb6fa0a
zoebeckman <zoe.beckman@yahoo.com>
2022-04-13 14:19:49 -0400

    minute preprocessing chagne

--- preprocessing.py
+++ preprocessing.py
@@ -1,13 +1,16 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('images/dog.jpg')
-dim = (10,10)
+def preprocessing(filepath):
+    img = cv2.imread(filepath)
+    dim = (10,10)
 
-# nearest neighbor interpolation looks best
-# will abstract to a function later when we have more code
-resized = cv2.resize(img, dim, fx=0.5, fy=0.5, interpolation=cv2.INTER_NEAREST)
+    # nearest neighbor interpolation looks best
+    # will abstract to a function later when we have more code
+    resized = cv2.resize(img, dim, fx=0.5, fy=0.5, interpolation=cv2.INTER_NEAREST)
 
-cv2.imshow('image',resized)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.imshow('image',resized)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+preprocessing('images/dog.jpg')
