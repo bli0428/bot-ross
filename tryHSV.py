d173e5715e333c8fe0bbecc0df69f7a96e2e7c6b
zoebeckman <zoe.beckman@yahoo.com>
2022-04-18 20:29:09 -0400

    not great

--- tryHSV.py
+++ tryHSV.py
@@ -12,26 +12,29 @@ default_rgb_values = [[254,231,31],[254,142,31], [241,33,17],[25,171,37],[16,119
 # might need to check black/white another way because white seems to be when saturation is like <.25
 # and black seems to be when value is below <.35 
 
-def get_HSVcolor(cx,cy):
-	pixel = img[cx][cy]
-	hsv = colorsys.rgb_to_hsv(pixel[0]/255,pixel[1]/255,pixel[2]/255)
+def get_HSVcolor(h,s,v):
+	#pixel = img[cx][cy]
+	#hsv = colorsys.rgb_to_hsv(pixel[0]/255,pixel[1]/255,pixel[2]/255)
+	#hsv = colorsys.rgb_to_hsv(r/255,g/255,b/255)
+	#hsv = colorsys.rgb_to_hsv(r,g,b)
+	hsv = [h,s,v]
 	print("Hue: ", hsv[0], " converted: ",    hsv[0]*360)
 	print("Saturation: ", hsv[1])
 	print("Value: ", hsv[2   ])
-	if hsv[1] < .25:
+	if hsv[1] < .13:
 		return default_rgb_values[6], names[6]
-	elif hsv[2] < .20:
+	elif hsv[2] < .13:
 		return default_rgb_values[7], names[7]
 	else:
-		if hsv[0] <= .033 or hsv[0] >= .9055:
+		if hsv[0] <= 18/360 or hsv[0] >= 342/360: #red
 			return default_rgb_values[0], names [0]
-		elif .033 < hsv[0] <= .1194 :
+		elif 18/360 < hsv[0] <= 38/360 : #orange
 			return default_rgb_values[1], names [1]
-		elif .1194 < hsv[0] <= .1833 :
+		elif 38/360 < hsv[0] <= 66/360 : #yellow
 			return default_rgb_values[2], names [2]
-		elif .1833 < hsv[0] <= .46 :
+		elif 67/360 < hsv[0] <= 169/360 : #green
 			return default_rgb_values[3], names [3]
-		elif .46 < hsv[0] <= .7388 :
+		elif 169/360 < hsv[0] <= 257/360 : #blue
 			return default_rgb_values[4], names [4]
 		else:
 			return default_rgb_values[5], names [5]
@@ -61,6 +64,8 @@ cnts = imutils.grab_contours(cnts)
 
 image = img
 
+img_hsv = cv2.cvtColor(img,cv2.COLOR_RGB2HSV)
+
 # loop over the contours
 for c in cnts:
 	# compute the center of the contour
@@ -69,11 +74,34 @@ for c in cnts:
 	cY = int(M["m01"] / (M["m00"]+ 1e-5))
 	# draw the contour and center of the shape on the image
 	# rgb_val, name = get_HSVcolor(cX,cY)
-	pixel = img[cX][cY]
-	print(pixel[0])
+	
+	r = 0
+	g = 0
+	b = 0
+	h = 0
+	s = 0
+	v = 0
+	for i in range(-10,10):
+		for j in range(-10,10):
+			newX = min(len(img[0])-1,max(0,cX+i))
+			newY = min(len(img)-1,max(0,cY+i))
+			pixel = img[newY][newX]
+			r += pixel[0]
+			g += pixel[1]
+			b += pixel[2]
+			pixel_hsv = img_hsv[newY][newX]
+			h += pixel_hsv[0]
+			s += pixel_hsv[1]
+			v += pixel_hsv[2]
+	r /= 400
+	g /= 400
+	b /= 400
+	h /= 400
+	s /= 400
+	v /= 400
 	cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-	cv2.circle(image, (cX, cY), 7, (int(pixel[0]), int(pixel[1]), int(pixel[2])), -1)
-	rgb_val, name = get_HSVcolor(cX,cY)
+	cv2.circle(image, (cX, cY), 7, (int(r), int(g), int(b)), -1)
+	rgb_val, name = get_HSVcolor(h,s,v)
 	#cv2.putText(image, "center", (cX - 20, cY - 20),
 		#cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 	cv2.putText(image, name, (cX - 20, cY - 20),
